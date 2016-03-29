fe72c92e4054e139becfcaecaf2de63645dc6e66
mikissa_s <mikissas@gmail.com>
2016-03-29 13:00:19 +0000

    fix_user_profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-before_action :set_User, only: [:edit, :update]
-
+  before_action :collect_user, only: [:edit, :update]
 
   def show # 追加
    @user = User.find(params[:id])
@@ -14,35 +13,37 @@ before_action :set_User, only: [:edit, :update]
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user # ここを修正
     else
       render 'new'
     end
   end
   
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password,:location, :profile,
-                                 :password_confirmation)
-  end
-
-def edit
+  def edit
+    @user = User.find(params[:id])
   end
   
   def update
-    if @User.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path, notice: 'メッセージを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
   end
   
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :location, :profile,
+                                 :password_confirmation)
+  end
   
-  def set_message
-    @User = Message.find(params[:id])
+  def collect_user 
+    user = User.find(params[:id])
+    redirect_to(root_url) if user != current_user
   end
 end
 
