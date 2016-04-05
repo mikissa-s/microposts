2438a4ea1d4ab55fe97473a304d912d99d3cbd3c
mikissa_s <mikissas@gmail.com>
2016-04-05 06:39:31 +0000

    fix_user_profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :location, allow_blank: true, length: { maximum: 0 }, on: :create
+  validates :location, allow_blank: true, length: { maximum: 30 }, on: :update
+  validates :profile, allow_blank: true, length: { maximum: 0 }, on: :create 
+  validates :profile, allow_blank: true, length: { maximum: 50 }, on: :update
 end
